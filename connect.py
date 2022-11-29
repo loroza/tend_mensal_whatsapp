481a76d326cd05fbe3d206525a9c2eab53803638
loroza <carlosedsansilva@gmail.com>
2022-11-29 12:02:56 -0300

    V.0.0.2

---
+++ connect.py
@@ -0,0 +1,48 @@
+class PostgreSQL():
+
+    pd = __import__('pandas')
+
+    #Read PostgreSQL
+    def read_postgres(name_table:str = None,
+                      schema:str = None,
+                      database:str = 'postgres',
+                      user:str = 'pecista',
+                      password:str = 'bueno123',
+                      host:str = '10.4.1.96',
+                      port:str = '5432',
+                      query:str='') -> pd.DataFrame:
+
+
+        import psycopg2
+        from psycopg2 import Error
+        import pandas as pd
+
+        if len(query) < 1:
+            query_execute = f'SELECT * FROM "{schema}".{name_table.upper()}'
+        else:
+            query_execute = f'SELECT * FROM "{schema}".{name_table.upper()} {query}'
+
+        try:
+            connection = psycopg2.connect(user=user,
+                                          password=password,
+                                          host=host,
+                                          port=port,
+                                          database=database)
+
+            cursor = connection.cursor()
+
+            cursor.execute(query_execute)
+
+            result_query = cursor.fetchall()
+            columns = [desc[0].upper() for desc in cursor.description]
+            dataframe = pd.DataFrame(result_query, columns=columns)
+
+        except (Exception, Error) as error:
+            print('Error', error)
+
+        finally:
+            if (connection):
+                cursor.close()
+                connection.close()
+        
+        return dataframe
